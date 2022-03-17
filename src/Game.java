3d32b59330ab9c84be796d2f0cbcdfe4347dcada
Paulo barbosa <paulobarbosah@hotmail.com>
2022-03-17 11:22:15 -0300

    capt 1 part 2

--- src/Game.java
+++ src/Game.java
@@ -4,15 +4,16 @@ import java.awt.Graphics;
 
 public class Game extends JPanel{
     public Game() {//construtor
-        setFocusable(true);
-        setLayout(null);
+        setFocusable(true);//para escultar os eventos 
+        setLayout(null);//para não usar outros componentes da classe
     }
 
+    //método sobrescrito ----
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        setBackground(Color.LIGHT_GRAY);
-        g.setColor(Color.RED);
-        g.fillOval(100,100,50,50);
+        super.paintComponent(g);//garantir o desenho na tela
+        setBackground(Color.LIGHT_GRAY);//atribui a cor ao fundo da tela
+        g.setColor(Color.RED);//define a cor do objeto para vermelho
+        g.fillOval(100,100,50,50);// desenha o objeto em tela (local da telaX, local da telaY, Largura, Altura)
     }
 }
--- src/Pricipal.java
+++ src/Pricipal.java
@@ -8,14 +8,13 @@ public class Pricipal {
     public Pricipal() {
         JFrame janela= new JFrame("Jogo2D");//criar um objeto do tipo JFrame como esse nome (" ")
         Game game= new Game();
-        game.setPreferredSize(new Dimension(LARGURA_TELA, ALTURA_TELA));
-        //janela.setSize(LARGURA_TELA,ALTURA_TELA);//definindo as dimensões da Janela
-        janela.getContentPane().add(game);
+        game.setPreferredSize(new Dimension(LARGURA_TELA, ALTURA_TELA));//nova forma de definindo as dimensões da Janela
+        janela.getContentPane().add(game);//adciona o obj (game) dentro da janela
         janela.setResizable(true);//possibilita o redimensionamento
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Garante que o app seja totalmente fechado
         janela.setLocation(100,100);// posição em que a janela vai ser redenizada na tela.
         janela.setVisible(true);//obrigatória para que a janela apareça
-        janela.pack();
+        janela.pack();// faz com quê  janela se adapte com ao tamanho do obj
     }
     public static void main(String[] args) {
         new Pricipal();
